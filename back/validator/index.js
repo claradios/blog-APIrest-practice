dbb1c732011ff485b193b0f1113193b41aec142c
Clara Dios <clara.dios.diez@gmail.com>
2020-01-13 21:52:48 +0100

    refactor validator

--- back/validator/index.js
+++ back/validator/index.js
@@ -1,32 +1,16 @@
 //Validador que verifica que el comentario no contiene ninguna palabra ofensiva.
-//https://stackoverflow.com/questions/2232934/whole-word-match-in-javascript
-//https://www.w3schools.com/js/js_regexp.asp
+
 module.exports = (text = '', wordsToCheck = []) => {
     const wrongWords = [];
     const lowerText = text.toLowerCase();
     wordsToCheck.forEach(offensiveWord => {
-        
         const { word } = offensiveWord;
-        const capitalized = word.charAt(0).toUpperCase() + word.slice(1);
+        const wordFound = new RegExp("\\b" + `${word}` + "\\b").test(lowerText);
+        const wordIsNotIncluded = !wrongWords.includes(offensiveWord);
 
-        const options = [
-            ` ${word} `,
-            ` ${word},`,
-            ` ${word}.`,
-            ` ${word}?`,
-            ` ${word}!`,
-            `${capitalized} `,
-            `${capitalized},`,
-            `${capitalized}.`,
-            `${capitalized}?`,
-            `${capitalized}!`,
-        ];
-      
-        options.forEach(opt => {
-            if (lowerText.includes(opt) || text.includes(opt)) {
-                !wrongWords.includes(offensiveWord) ? wrongWords.push(offensiveWord) : null
-            }
-        });
+        if ( wordFound && wordIsNotIncluded) {            
+             wrongWords.push(offensiveWord);
+        }
     });
 
     return wrongWords;
