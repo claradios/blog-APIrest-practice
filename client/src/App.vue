8a6e1a181989dbeb0f01a9e33d21d0619da360fb
Clara Dios <clara.dios.diez@gmail.com>
2020-01-20 14:39:17 +0100

    delete helloworld component from app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,8 @@
 <template>
   <div id="app">
      <router-view/>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
