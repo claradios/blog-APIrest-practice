3bcd4dbeff0424f02fe128d77da1f1a54ee608bf
Clara Dios <clara.dios.diez@gmail.com>
2020-01-30 09:53:47 +0100

    install vuetify and fonts ond index

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,34 +1,56 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app>
+    <router-view/>
+    <!-- <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content> -->
+  </v-app>
 </template>
+
 <script>
 
-// import userInfo from '@/store/'
 export default {
-  name: 'app'
-  // beforeMount () {
-  //   console.log('montando')
-  //   userInfo.getFromLocalStorage()
-  // }
-  // ,
-  // beforeDestroy () {
-  //   localStorage.clear()
-  // }
+  name: 'App',
+
+  data: () => ({
+    //
+  })
 }
 </script>
-<style>
-html,
-body,
-#app {
-  margin: 0;
-  /* margin-top: 60px; */
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  font-size: 14px;
-}
-</style>
