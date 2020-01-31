521b1bfe5a6cf1e31aab67740217e296a84bc867
Clara Dios <clara.dios.diez@gmail.com>
2020-01-31 09:24:09 +0100

    simplify login route

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
+// import Login from '../views/Login.vue'
+import TheMainLogin from '../components/TheMainLogin.vue'
 import Read from '../views/Read.vue'
 import NewPost from '../views/NewPost.vue'
 import Signup from '../views/Signup.vue'
@@ -42,7 +43,7 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    component: Login
+    component: TheMainLogin
   },
   {
     path: '/admin/settings/offensivewords',
