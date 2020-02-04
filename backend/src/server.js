52144bc3b543168dbf66c8d3c756330dfc4d9ddf
Clara Dios <clara.dios.diez@gmail.com>
2020-02-04 12:38:22 +0100

    adjust bodyParser limit to 50mb to upload bigger files

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,12 +21,18 @@ const jwtOpts = {
     secretOrKey: SECRET_KEY
 }
 
+
 const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.use(passport.initialize());
 app.use(cors());
 app.use(express.json());
 app.use('/', controller);
+
 
 
 async function verify(username, password, done) {
