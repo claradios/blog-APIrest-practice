b0e57ef771944aa9bb30f0552799f85e77b37a42
Clara Dios <clara.dios.diez@gmail.com>
2020-02-04 13:28:47 +0100

    add fileReader to user image on sing up form

--- client/src/components/TheMainSignup.vue
+++ client/src/components/TheMainSignup.vue
@@ -42,33 +42,40 @@
                   class="password"
                   required
                 ></v-text-field>
-               <v-text-field
-                  label="Picture"
-                  placeholder="Enter url"
-                  name="picture"
-                  prepend-icon="mdi-file-image"
-                  type="text"
+                <v-file-input
+                  label="upload picture"
+                  name="profile"
+                  type="file"
                   v-model="userImage"
+                  id="file"
+                  class="inputfile"
+                  @change="handleUploadImage"
                   required
-                ></v-text-field>
+                ></v-file-input>
+                <v-avatar tile="true" size="56px" color="grey lighten-4">
+                  <img :src="userImage" alt="avatar" />
+                </v-avatar>
+                <figure class="image is-32x32">
+                  <div :style="{ backgroundImage: 'url(' + userImage + ')' }"></div>
+                </figure>
               </v-form>
-              </v-card-text>
+            </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn color="primary" @click="sign()">SIGN UP</v-btn>
             </v-card-actions>
-          </v-card >
-           <v-card class="text-center" color="transparent" elevation=0>
-              <div  v-if="successMsg" class="success">
-                <p>Conseguido!</p>
-                <p>
-                  <router-link :to="'/login'">
+          </v-card>
+          <v-card class="text-center" color="transparent" elevation="0">
+            <div v-if="successMsg" class="success">
+              <p>Conseguido!</p>
+              <p>
+                <router-link :to="'/login'">
                   <strong>Haz log in!</strong>
-                  </router-link>
-                </p>
-              </div>
-              <p v-if="errorMsg.length > 0" class="info-message">{{errorMsg}}</p>
-            </v-card>
+                </router-link>
+              </p>
+            </div>
+            <p v-if="errorMsg.length > 0" class="info-message">{{errorMsg}}</p>
+          </v-card>
         </v-flex>
       </v-layout>
     </v-container>
@@ -106,11 +113,21 @@ export default {
 
         this.errorMsg = error.message
       }
+    },
+    handleUploadImage (ev) {
+      const files = ev
+      if (!files) return
+
+      const reader = new FileReader()
+      reader.readAsDataURL(files)
+      reader.onload = ev => {
+        this.userImage = ev.target.result
+      }
+      document.querySelector('#file').value = ''
     }
   }
 }
 </script>
 
 <style lang="scss">
-
 </style>
